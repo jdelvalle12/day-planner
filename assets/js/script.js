5a1b6405da27e2fb23ab85d5232b8f755f095903
jdelvalle12 <104731082+jdelvalle12@users.noreply.github.com>
2022-12-07 21:53:57 -0500

    new codes added onto JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var timeDisplayEl = $('#time-display');
+var currentDayEl = $('#currentDay');
+var hourXEl = $('#hour-x');
+
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+function readSchedulerFromStorage() {
+  var scheduler = localStorage.getItem('scheduler');
+  if (scheduler) {
+    scheduler = JSON.parse(scheduler);
+  } else {
+    scheduler = [];
+  }
+  return scheduler;
+}
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -22,3 +41,4 @@ $(function () {
     // TODO: Add code to display the current date in the header of the page.
   });
   
+  //saveBtn.addEventListener('click', )
